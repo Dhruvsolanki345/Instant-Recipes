bd27235233684424a85c7c28f6b6dc91920af066
DhruvSolanki345 <dhruvsolanki753@gmail.com>
2021-07-30 19:41:52 +0530

    Updates

---
+++ precache-manifest.d551509b19eeeb7517a316c2deb96112.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571444457145ecbca2ccaa3cf2ae00c4",
+    "url": "/Instant-Recipes/index.html"
+  },
+  {
+    "revision": "3caeabb53fd082be4a42",
+    "url": "/Instant-Recipes/static/css/main.087e5abb.chunk.css"
+  },
+  {
+    "revision": "9343f5c7eb92e0913395",
+    "url": "/Instant-Recipes/static/js/2.83f05f7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Instant-Recipes/static/js/2.83f05f7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3caeabb53fd082be4a42",
+    "url": "/Instant-Recipes/static/js/main.41c7fd6f.chunk.js"
+  },
+  {
+    "revision": "0681ae9e10f87d3e949e",
+    "url": "/Instant-Recipes/static/js/runtime-main.5525452e.js"
+  }
+]);
